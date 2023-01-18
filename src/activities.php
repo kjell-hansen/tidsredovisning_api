db9fe732d046dbcbfcb44f66f067d66ee7606162
Kjell Hansen (S1-KJHA-2905) <kjell.hansen@gymnasium.ax>
2023-01-18 14:16:04 +0200

    Hämta enskild funkar och alla testcases ok

--- src/activities.php
+++ src/activities.php
@@ -63,7 +63,35 @@ function hamtaAlla(): Response {
  * @return Response
  */
 function hamtaEnskild(int $id): Response {
-    return new Response("Hämta aktivitet $id", 200);
+    // Kontrollera indata
+    $kollatID = filter_var($id, FILTER_VALIDATE_INT);
+    if (!$kollatID || $kollatID < 1) {
+        $out = new stdClass();
+        $out->error = ["Felaktig indata", "$id är inget giltigt heltal"];
+        return new Response($out, 400);
+    }
+
+    // Koppla databas och hämta post
+    $db= connectDb();
+    $stmt=$db->prepare("SELECT id, activity FROM activities where id=:id");
+    if (!$stmt->execute(["id"=>$kollatID])) {
+        $out=new stdClass();
+        $out->error=["Fel vid läsning från databasen", implode(",", $db->errorInfo()  )  ];
+        return new Response($out,400);
+    }
+    
+    // Sätt utdata och returnera 
+    if($row=$stmt->fetch()) {
+        $out=new stdClass();
+        $out->id=$row["id"];
+        $out->activity=$row["activity"];
+        return new Response($out);
+    } else {
+        $out=new stdClass();
+        $out->error=["Hittade ingen post med id=$kollatID"];
+        return new Response($out, 400);
+    }
+
 }
 
 /**
--- test/testActivities.php
+++ test/testActivities.php
@@ -54,7 +54,7 @@ function test_HamtaAllaAktiviteter(): string {
             }
         }
     } catch (Exception $ex) {
-        $retur .="<p class='error'>Något gick fel, meddelandet säger:<br> {$ex->getMessage()}</p>";
+        $retur .= "<p class='error'>Något gick fel, meddelandet säger:<br> {$ex->getMessage()}</p>";
     }
 
     return $retur;
@@ -66,8 +66,43 @@ function test_HamtaAllaAktiviteter(): string {
  */
 function test_HamtaEnAktivitet(): string {
     $retur = "<h2>test_HamtaEnAktivitet</h2>";
-    $retur .= "<p class='ok'>Testar hämta en aktivitet</p>";
-    $retur .= "<p class='error'>Det här testet gick fel!</p>";
+    try {
+        // Testa negativt tal
+        $svar = hamtaEnskild(-1);
+        if ($svar->getStatus() === 400) {
+            $retur .= "<p class='ok'>Hämta enskild med negativt tal ger förväntat svar 400</p>";
+        } else {
+            $retur .= "<p class='error'>Hämta enskild med negativt tal ger {$svar->getStatus()} "
+                    . "inte förväntat svar 400</p>";
+        }
+        // Testa för stort tal
+        $svar = hamtaEnskild(100);
+        if ($svar->getStatus() === 400) {
+            $retur .= "<p class='ok'>Hämta enskild med stort tal ger förväntat svar 400</p>";
+        } else {
+            $retur .= "<p class='error'>Hämta enskild med stort (100) tal ger {$svar->getStatus()} "
+                    . "inte förväntat svar 400</p>";
+        }
+        // Testa bokstäver
+        $svar = hamtaEnskild((int) "sju");
+        if ($svar->getStatus() === 400) {
+            $retur .= "<p class='ok'>Hämta enskild med bokstäver ger förväntat svar 400</p>";
+        } else {
+            $retur .= "<p class='error'>Hämta enskild med bokstäver ('sju') tal ger {$svar->getStatus()} "
+                    . "inte förväntat svar 400</p>";
+        }
+        // Testa giltigt tal
+        $svar = hamtaEnskild(3);
+        if ($svar->getStatus() === 200) {
+            $retur .= "<p class='ok'>Hämta enskild med 3 ger förväntat svar 200</p>";
+        } else {
+            $retur .= "<p class='error'>Hämta enskild med 3 ger {$svar->getStatus()} "
+                    . "inte förväntat svar 200</p>";
+        }
+    } catch (Exception $ex) {
+        $retur .= "<p class='error'>Något gick fel, meddelandet säger:<br> {$ex->getMessage()}</p>";
+    }
+
     return $retur;
 }
 
